06e4e1e07b9e329de7e2a5b192f4d3f1d3403138
Jatin Arora <jatin.19.arora@gmail.com>
2017-10-14 08:38:40 +0530

    initial commit

---
+++ backend/src/main/java/com/igt/todoapp/models/Todo.java
@@ -0,0 +1,75 @@
+package com.igt.todoapp.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotBlank
+    @Size(max=100)
+    private String title;
+
+    private Boolean completed = false;
+
+    private Date createdAt = new Date();
+
+    public Todo() {
+        super();
+    }
+
+    public Todo(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Todo[id=%s, title='%s', completed='%s']",
+                id, title, completed);
+    }
+}
